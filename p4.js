7f0aff26454043273993acb0f95f42b6b5796524
aa <133742362+yuzhouzhibao@users.noreply.github.com>
2024-03-12 23:35:58 +0100

    Update p4.js

--- p4.js
+++ p4.js
@@ -1,25 +1,29 @@
+let isGoingBack = false;  // 新增标志位
+
 document.addEventListener('keydown', function (event) {
-    if (event.code === 'ArrowLeft' || event.code === 'ArrowRight') {
-        // 左箭头键或右箭头键，执行切换页面逻辑
-        if (event.code === 'ArrowLeft') {
-            prev(); // 左箭头键
-        } else if (event.code === 'ArrowRight') {
-            next(); // 右箭头键
-        }
-    } else if (event.code === 'Space') {
-        // 空格键，执行跳转到指定页面逻辑
-        jumpToPage(currIndex);
-    } else if (event.code === 'KeyP') {
+    if (!isGoingBack && event.key === 'p') {
         // 'p' 键，执行返回上一页逻辑
         goBack();
     }
 });
 
 function goBack() {
-    // 编写返回上一页的逻辑
-    // 这里简单的使用浏览器的后退功能
-    window.history.back();
+    if (!isGoingBack) {
+        isGoingBack = true;  // 设置标志位，防止多次触发
+        console.log('Going back...');
+        
+        // 编写返回上一页的逻辑
+        // 这里简单的使用浏览器的后退功能
+        window.history.back();
+
+        // 一定时间后重置标志位，允许再次触发
+        setTimeout(function () {
+            isGoingBack = false;
+        }, 1000);  // 1秒后重置标志位
+    }
 }
+
+
 var mouse = document.querySelector(".mouse");
 window.addEventListener("mousemove",function(event){
     mouse.style.left = event.clientX - mouse.offsetWidth/2+"px";
